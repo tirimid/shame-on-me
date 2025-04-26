a3629a15768f2364f7a8a56979d433227bbb58ca
tirimid <tirimid@tirimid.net>
2025-04-26 18:47:03 +0400

    working on making windows build work

---
+++ linux_build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+INCLUDE="-I. -Isrc"
+DEFINES="-DPROFILE"
+WARNINGS="-Wall -Wextra -Wshadow"
+LIBRARIES="$(pkg-config --cflags --libs sdl2 gl SDL2_mixer SDL2_ttf SDL2_image cglm glew) -lm"
+CFLAGS="-std=c99 -pedantic -O3"
+
+CC=gcc
+CFLAGS_FULL="$INCLUDE $DEFINES $WARNINGS $CFLAGS $LIBRARIES"
+
+echo "[$0] build: resources" >&2
+make -s -C img > /dev/null
+make -s -C model > /dev/null
+make -s -C shader > /dev/null
+make -s -C font > /dev/null
+
+echo "[$0] build: compilation" >&2
+$CC -o shame-on-me src/main.c $CFLAGS_FULL
+if [ $? -ne 0 ]
+then
+	echo "[$0] build: failed to compile!" >&2
+	exit 1
+fi
+
+echo "[$0] build: finished successfully" >&2
